11ee23e15ba512e8236942c4cb8de8f1be6f9aaa
Shubham9454 <shubhamy.9454@gmail.com>
2025-07-20 22:44:49 +0530

    Built the user's connection API

--- src/routes/user.js
+++ src/routes/user.js
@@ -29,9 +29,35 @@ userRouter.get("/user/requests/received" , userAuthentication , async (req , res
 
 });
 
-userRouter.get("/" , userAuthentication , (req , res) =>{
+userRouter.get("/user/connections" , userAuthentication , async (req , res) =>{
 
   try{
+
+    const loggedInUser = req.user;
+
+    const connectionData = await connectionModel.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted"},
+        {fromUserId: loggedInUser._id, status: "accepted"}
+    ]}).populate("fromUserId" , ["firstName" , "lastName"])
+       .populate("toUserId" , ["firstName" , "lastName"]);
+
+    if(connectionData.length == 0) {
+      res.send("You have no connections");
+    }
+    
+    const finalData = connectionData.map((row) =>{
+      if(row.fromUserId._id.equals(loggedInUser._id)){
+      return row.toUserId;
+    }
+    return row.fromUserId;
+     
+    });
+    
+    res.json({
+      message: "Your connections list are below:",
+      data: finalData
+    });
 
   } catch(err){
     res.status(400).send("Something went wrong ! with error: " + err.message);
